8e0500d399367ffed694811399b7a540f6b3444f
Stephen Scott <me@suchipi.com>
2017-02-03 12:00:13 -0700

    Fix public path in github pages

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ export default class App extends React.Component {
   render() {
     return (
       <LockScreen
-        wallpaperPath="/react_wallpaper.png"
+        wallpaperPath="react_wallpaper.png"
         userInfoMessage="This is Tim's phone. If found, please give it back to him. He will be sad without it"
         onUnlocked={() => alert("unlocked!")}
       />
